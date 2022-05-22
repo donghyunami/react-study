6916c0429bc9705e8cda72b0def02e986970d571
Donghyun Lee <skyblue5030@naver.com>
2022-05-22 23:19:22 +0900

    리액트에서 undefined 처리

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
 function App() {
-  const name = undefined;
-  return name;
-}
+  // const username = undefined;
+  const username = undefined;
+  // return username || '빈값입니다.'; 이런식으로 랜더링해야 한다.
 
+  /* jsx 안에서는 undefined 랜더링 가능 */
+  return <div>{username} </div>;
+}
 export default App;
