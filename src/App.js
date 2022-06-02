8dea288c30704dbeb11ab7b2418f55fceb0e4100
Donghyun Lee <skyblue5030@naver.com>
2022-06-02 22:28:30 +0900

    다른 컴포넌트 만들기 code 업로드

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React from 'react';
-import './App.css';
+import Hello from './Hello';
 
 function App() {
-  const name = '리액트';
-
-  const style = {
-    background: 'black',
-    color: 'aqua',
-    fontSize: '32px', // font-size → fontSize
-    fontWeight: 'bold', //font-weight → fontWeight
-    padding: 16, // 단위를 생략하면 px로 지정됨
-  };
   return (
     <div>
-      <h1 style={style}>{name}</h1>
-      <nav>
-        <ul className="menu">
-          <li className="item">HTML</li>
-          <li className="item">CSS</li>
-          <li className="item">JavaScript</li>
-          <li className="item">React</li>
-        </ul>
-      </nav>
+      <Hello />
     </div>
   );
 }
+
 export default App;
