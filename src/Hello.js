387a1a05cd47e905fbcdd5897832f7f48ef5f9f6
Donghyun Lee <skyblue5030@naver.com>
2022-06-05 16:40:49 +0900

    props에 대해서(1)

--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-function Hello() {
-  return <div>안녕하세요</div>;
+function Hello({ color, name }) {
+  return (
+    <div>
+      안녕하세요 제 이름은 <span style={{ color }}>{name}</span>입니다.
+    </div>
+  );
 }
 
+Hello.defaultProps = {
+  name: '이름 미지정',
+  color: 'pink',
+};
 export default Hello;
