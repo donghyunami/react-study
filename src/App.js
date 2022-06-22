0a7e59de250b5afc97839169e2f37f065ff9a4df
Donghyun Lee <skyblue5030@naver.com>
2022-06-22 19:24:28 +0900

    Hook, useState 다루기

--- src/App.js
+++ src/App.js
@@ -1,79 +1,22 @@
-import React, { useRef, useState } from 'react';
-import UserList from './components/UserList';
-import CreateUser from './components/CreateUser';
+import React, { useState } from 'react';
 
 function App() {
-  const [inputs, setInputs] = useState({
-    username: '',
-    email: '',
-  });
-  const { username, email } = inputs;
-  const onChange = e => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value,
-    });
-  };
-  const [users, setUsers] = useState([
-    {
-      id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com',
-    },
-    {
-      id: 2,
-      username: 'tester',
-      email: 'tester@example.com',
-    },
-    {
-      id: 3,
-      username: 'liz',
-      email: 'liz@example.com',
-    },
-  ]);
+  const [number, setNumber] = useState(0);
 
-  const nextId = useRef(4);
-  const onCreate = () => {
-    const user = {
-      id: nextId.current,
-      username,
-      email,
-    };
-    setUsers(users.concat(user));
-
-    setInputs({
-      username: '',
-      email: '',
-    });
-    nextId.current += 1;
+  const onIncrease = () => {
+    setNumber(prevNumber => prevNumber + 1);
   };
 
-  const onEdit = (text, id) => {
-    console.log({ text, id });
-    setUsers(
-      users.map(user => {
-        return user.id === id ? { ...user, username: text } : user;
-      }),
-    );
-  };
-
-  const onRemove = id => {
-    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
-    // = user.id 가 id 인 것을 제거함
-    setUsers(users.filter(user => user.id !== id));
+  const onDecrease = () => {
+    setNumber(prevNumber => prevNumber - 1);
   };
 
   return (
-    <>
-      <CreateUser
-        username={username}
-        email={email}
-        onChange={onChange}
-        onCreate={onCreate}
-      />
-      <UserList users={users} onRemove={onRemove} onEdit={onEdit} />
-    </>
+    <div>
+      <h1>{number}</h1>
+      <button onClick={onIncrease}>+1</button>
+      <button onClick={onDecrease}>-1</button>
+    </div>
   );
 }
 
